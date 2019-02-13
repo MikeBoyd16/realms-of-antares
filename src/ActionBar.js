90ee13c819c15440d25a5685060ba9c2f2924206
unknown <mikeboyd165@gmail.com>
2019-02-12 20:38:13 -0800

    Merge Location and ActionBar into one component

--- src/ActionBar.js
+++ src/ActionBar.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import Location from './Location';
+import ActionButton from './ActionButton';
 import './ActionBar.css';
+import data from './data.json';
 
 class ActionBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentLocation: data["Riverstar"]
+        };
+        this.createActions = this.createActions.bind(this);
+    }
+    handleAction = (newLocation) => { 
+        this.setState({ currentLocation: data[newLocation] }) 
+    }
+    createActions() {
+        return(
+            <div>
+            {
+                Object.entries(this.state.currentLocation["actions"]).map(([key, value]) => 
+                <ActionButton actionName={value["name"]} onClick={() => this.handleAction(value["value"])} />)
+            }
+            </div>
+        );
+    }
     render() {
         return(
             <div className="ActionBar">
-                <div className="Wrapper">
-                    <Location />
+                <div className="wrapper">
+                    {this.createActions()}
                 </div>
             </div>
         );
