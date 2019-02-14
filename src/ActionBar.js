1b287d10bc1810b0066a71388d98dfce6f3b09f8
unknown <mikeboyd165@gmail.com>
2019-02-14 14:07:22 -0800

    Display empty ActionButton components in the ActionBar if there are fewer than six actions

--- src/ActionBar.js
+++ src/ActionBar.js
@@ -4,20 +4,42 @@ import './ActionBar.css';
 import { GameWorldContext } from './GameWorldContext';
 
 class ActionBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            actionNum: 0
+        }
+        this.createActions = this.createActions.bind(this);
+        this.createEmptyActions = this.createEmptyActions.bind(this);
+    }
+    createActions() {
+        return(
+            <GameWorldContext.Consumer>
+                {({ location }) => (
+                    <div>
+                    {
+                        Object.entries(location["actions"]).map(([key, value]) => 
+                        <ActionButton key={key} actionName={value["name"]} actionValue={value["value"]} actionMessage={value["message"]} />)
+                    }
+                    {this.createEmptyActions(Object.keys(location["actions"]).length)}
+                    </div>
+                )}
+            </GameWorldContext.Consumer>
+        ); 
+    }
+    createEmptyActions(numChildren) {
+        var emptyActions = [];
+        while(numChildren < 6) {
+            emptyActions.push(<ActionButton key={numChildren+1} actionName=" " actionValue="empty" actionMessage="empty" />);
+            numChildren += 1;
+        }
+        return(emptyActions);
+    }
     render() {
         return(
             <div className="ActionBar">
                 <div className="wrapper">
-                    <GameWorldContext.Consumer>
-                        {({ location }) => (
-                            <div>
-                            {
-                                Object.entries(location["actions"]).map(([key, value]) => 
-                                <ActionButton key={key} actionName={value["name"]} actionValue={value["value"]} actionMessage={value["message"]} />)
-                            }
-                            </div>
-                        )}
-                    </GameWorldContext.Consumer>   
+                    {this.createActions()}
                 </div>
             </div>
         );
