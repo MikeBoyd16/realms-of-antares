7c72b274417e5dd70dc7ae5d16861c47971c19f4
unknown <mikeboyd165@gmail.com>
2019-02-18 16:56:07 -0800

    Add the Navigation component and styles

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Import App component styles
+import '../css/Navigation.css';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div id="navigation">
+                Navigation
+            </div>
+        );
+    }
+}
+
+export default Navigation;
