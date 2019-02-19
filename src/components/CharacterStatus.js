0228d79fdb780cbe67fd08a09bacd4a8a38de09f
unknown <mikeboyd165@gmail.com>
2019-02-18 16:59:15 -0800

    Add the CharacterStatus component and styles

---
+++ src/components/CharacterStatus.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Import CharacterStatus component styles
+import './CharacterStatus.css';
+
+class CharacterStatus extends Component {
+    render() {
+        return (
+            <div id="characterStatus">
+                Character Status
+            </div>
+        );
+    }
+}
+
+export default CharacterStatus;
