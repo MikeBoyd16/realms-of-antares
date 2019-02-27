98b74bd145971e82fb9e1420c98fa0665a227ce9
unknown <mikeboyd165@gmail.com>
2019-02-27 07:36:12 -0800

    Add TitleMenu component

---
+++ src/components/TitleMenu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// Import game context
+import { GameWorldContext } from '../GameWorldContext';
+
+// Import component styles
+import '../appStyles.css';
+
+class TitleMenu extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TitleMenu;
