5c32b5802eedeabf8a5f0d439fc293acb99408af
unknown <mikeboyd165@gmail.com>
2019-02-27 07:44:14 -0800

    Add CharacterCreation component

---
+++ src/components/CharacterCreation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// Import game context
+import { GameWorldContext } from '../GameWorldContext';
+
+// Import component styles
+import '../appStyles.css';
+
+class CharacterCreation extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CharacterCreation;
