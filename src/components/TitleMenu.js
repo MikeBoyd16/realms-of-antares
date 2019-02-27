aa981d211dfdb8f694d09ac939a4a23a305f171a
unknown <mikeboyd165@gmail.com>
2019-02-27 12:57:17 -0800

    Add TitleMenu elements and styles

--- src/components/TitleMenu.js
+++ src/components/TitleMenu.js
@@ -10,7 +10,13 @@ class TitleMenu extends Component {
     render() {
         return(
             <div id="titleMenu">
-                Title Menu
+                <div id="titlePanel">
+                    <img src={require("../images/title.PNG")} />
+                </div>
+                <div id="actionPanel">
+                    <button>New Game</button>
+                    <button>Import Save</button>
+                </div>
             </div>
         );
     }
